f94ffb2dd59db379331bfa613f79f07877973d3d
eagle <harshalmotiraya@gmail.com>
2026-01-23 11:23:50 +0530

    update new file

--- loops/counPositiveNum.py
+++ loops/counPositiveNum.py
@@ -5,5 +5,9 @@ for num in number:
     if num > 0:
         postive_number_count = postive_number_count + 1
 
+length = len(number)
+length2 = len(str(postive_number_count))
+
 print("final count of positive number is :",postive_number_count)
-print(length)
+print("length of count is :",number)
+print("length of count is :",length2)
